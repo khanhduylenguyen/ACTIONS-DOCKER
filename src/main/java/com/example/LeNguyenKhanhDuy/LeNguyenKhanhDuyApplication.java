3817b07fd581123cbea54588f5e9f0ff8056897b
khanhduylenguyen <khanhduylenguyen74@gmail.com>
2026-03-20 15:46:27 +0700

    first commit

---
+++ src/main/java/com/example/LeNguyenKhanhDuy/LeNguyenKhanhDuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.LeNguyenKhanhDuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeNguyenKhanhDuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeNguyenKhanhDuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LeNguyenKhanhDuy/LeNguyenKhanhDuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LeNguyenKhanhDuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeNguyenKhanhDuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
